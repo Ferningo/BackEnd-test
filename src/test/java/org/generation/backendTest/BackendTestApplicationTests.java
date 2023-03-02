6f11c1566428f443f57577b578728cefd3489055
Jose <jose.mar.123@hotmail.com>
2023-03-01 23:57:44 -0600

    Add starter spring initlzr files

---
+++ src/test/java/org/generation/backendTest/BackendTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.backendTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
